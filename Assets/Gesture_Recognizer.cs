58ae907f5c64fce44b07865af52e4b1cd284cb60
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-12-05 12:30:27 +0100

    Added "gesture recognition" and voice chat as well to a VR environment that I can control

---
+++ Assets/Gesture_Recognizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gesture_Recognizer : MonoBehaviour {
+
+    bool rightControllerActive = false;
+    bool leftControllerActive = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        List<GameObject> touchingObjects = this.GetComponent<VRTK.VRTK_InteractableObject>().GetTouchingObjects();
+    
+        foreach(GameObject g in touchingObjects)
+        {
+            if(g.name == "RightController")
+            {
+                rightControllerActive = true;
+            }
+            if(g.name == "LeftController")
+            {
+                leftControllerActive = true;
+            }
+        }
+
+
+	}
+}
