5d05bf6037e0767de6461d8fe8d72eb418434c6e
Almightyquad <christerpsomby@gmail.com>
2017-12-07 14:07:38 +0100

    Finally got the voice chat to work again. I was not actually starting the voice recording which wasnt helping

---
+++ Assets/Starter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+namespace ExitGames.Demos.DemoPunVoice
+{
+    public class Starter : MonoBehaviour
+    {
+
+        private PhotonVoiceRecorder rec;
+
+        void OnEnable()
+        {
+            CharacterInstantiation.CharacterInstantiated += CharacterInstantiation_CharacterInstantiated;
+        }
+
+        private void CharacterInstantiation_CharacterInstantiated(GameObject character)
+        {
+            rec = character.GetComponent<PhotonVoiceRecorder>();
+            rec.enabled = true;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
