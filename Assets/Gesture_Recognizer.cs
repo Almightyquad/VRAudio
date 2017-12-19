d1258e06b7ffceae4289905e04f9e80213e6a287
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-12-19 03:05:23 +0100

    Added floating UI element to display whether or not a controller is in the collided area. Also updated bib

--- Assets/Gesture_Recognizer.cs
+++ Assets/Gesture_Recognizer.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gesture_Recognizer : MonoBehaviour {
 
     bool rightControllerActive = false;
     bool leftControllerActive = false;
-
+    GameObject canvas;
+    GameObject left;
+    GameObject right;
+    GameObject both;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        canvas = GameObject.Find("ControllerCanvas");
+        right = canvas.transform.FindChild("Right").gameObject;
+        left = canvas.transform.FindChild("Left").gameObject;
+        both = canvas.transform.FindChild("Both").gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,10 +29,39 @@ public class Gesture_Recognizer : MonoBehaviour {
             {
                 rightControllerActive = true;
             }
-            if(g.name == "LeftController")
+            else if(g.name == "LeftController")
             {
                 leftControllerActive = true;
             }
+        }
+        if (touchingObjects.Count == 0)
+        {
+            rightControllerActive = false;
+            leftControllerActive = false;
+        }
+        if (rightControllerActive == true)
+        {
+            right.transform.FindChild("Text").GetComponent<Text>().text = "Right: True";
+        }
+        else
+        {
+            right.transform.FindChild("Text").GetComponent<Text>().text = "Right: False";
+        }
+        if (leftControllerActive == true)
+        {
+            left.transform.FindChild("Text").GetComponent<Text>().text = "Left: True";
+        }
+        else
+        {
+            left.transform.FindChild("Text").GetComponent<Text>().text = "Left: False";
+        }
+        if (rightControllerActive == true && leftControllerActive == true)
+        {
+            both.transform.FindChild("Text").GetComponent<Text>().text = "Both: True";
+        }
+        else
+        {
+            both.transform.FindChild("Text").GetComponent<Text>().text = "Both: false";
         }
 	}
 }
